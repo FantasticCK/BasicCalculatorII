ffa4257a8d7e5553d0c2dd72b2c5ce426c3bce0b
CK <kainingc@usc.edu>
2020-01-27 11:25:14 -0800

    add without stack solution

--- src/com/CK/Main.java
+++ src/com/CK/Main.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class Main {
 
     public static void main(String[] args) {
-        String s = " -3+5 / 2 * 9";
+        String s = " -3+5 / -2 * 9";
 //        String[] cArr = s.split("\\w");
 //        System.out.println(cArr);
         Solution solution = new Solution();
@@ -47,5 +47,44 @@ class Solution {
             re += i;
         }
         return re;
+    }
+}
+
+class Solution {
+    public int calculate(String s) {
+        if (s.length() == 0)
+            return 0;
+        s = s + "$";
+        int prevNum = 0, currNum = 0, sum = 0;
+        char prevOpt = '+';
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ')
+                continue;
+            if (Character.isDigit(c)) {
+                currNum = currNum * 10 + c - '0';
+                continue;
+            }
+
+            switch(prevOpt){
+                case '+':
+                    sum += prevNum;
+                    prevNum = currNum;
+                    break;
+                case '-':
+                    sum += prevNum;
+                    prevNum = -currNum;
+                    break;
+                case '*':
+                    prevNum *= currNum;
+                    break;
+                case '/':
+                    prevNum /= currNum;
+                    break;
+            }
+            currNum = 0;
+            prevOpt = c;
+        }
+        return sum + prevNum;
     }
 }
